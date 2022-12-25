93d5f90a89ff93737420ddc88e50ccb87e4a2af4
Dor Shtaif <dor@rapidapi.com>
2022-12-25 22:06:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# my-general-test-ground-project
---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = myTestFunction;
+
+function myTestFunction() {
+  const result = `Hello from my general test ground project, it is ${new Date().toISOString()}`;
+  return result;
+}
